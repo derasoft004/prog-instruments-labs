f179dcd14167904a1013dd7b28227a19afd213c6
Egor <derasoft004@gmail.com>
2024-11-10 22:52:45 +0400

    lab_3-commit5: add some dock-strings

--- lab_3/checksum.py
+++ lab_3/checksum.py
@@ -9,15 +9,7 @@ from regulator import load_regular_expressions, get_data_from_csv, match_express
 
 def calculate_checksum(row_numbers: List[int]) -> str:
     """
-    Вычисляет md5 хеш от списка целочисленных значений.
-
-    ВНИМАНИЕ, ВАЖНО! Чтобы сумма получилась корректной, считать, что первая строка с данными csv-файла имеет номер 0
-    Другими словами: В исходном csv 1я строка - заголовки столбцов, 2я и остальные - данные.
-    Соответственно, считаем что у 2 строки файла номер 0, у 3й - номер 1 и так далее.
-
-    Надеюсь, я расписал это максимально подробно.
-    Хотя что-то мне подсказывает, что обязательно найдется человек, у которого с этим возникнут проблемы.
-    Которому я отвечу, что все написано в докстринге ¯\_(ツ)_/¯
+    The function calculates checksum using list numbers of nonvalid strings
 
     :param row_numbers: список целочисленных номеров строк csv-файла, на которых были найдены ошибки валидации
     :return: md5 хеш для проверки через github action
@@ -29,12 +21,7 @@ def calculate_checksum(row_numbers: List[int]) -> str:
 @context_manager_wrapper
 def serialize_result(variant: int, checksum: str) -> None:
     """
-    Метод для сериализации результатов лабораторной пишите сами.
-    Вам нужно заполнить данными - номером варианта и контрольной суммой - файл, лежащий в папке с лабораторной.
-    Файл называется, очевидно, result.json.
-
-    ВНИМАНИЕ, ВАЖНО! На json натравлен github action, который проверяет корректность выполнения лабораторной.
-    Так что не перемещайте, не переименовывайте и не изменяйте его структуру, если планируете успешно сдать лабу.
+    The function serializes result that got using func 'calculate_checksum'
 
     :param variant: номер варианта
     :param checksum: контрольная сумма, вычисленная через calculate_checksum()
--- lab_3/wrappers.py
+++ lab_3/wrappers.py
@@ -4,6 +4,12 @@ logging.basicConfig(level=logging.INFO)
 
 
 def context_manager_wrapper(func):
+    """
+    The wrapper using to wrap functions that open files
+
+    :param func:
+    :return:
+    """
     def wrapper(*args, **kwargs):
         try:
             return func(*args, **kwargs)
